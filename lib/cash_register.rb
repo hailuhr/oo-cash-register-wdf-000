8709414853182fb5bc1bcbad1b53d64d2e6ed650
Flatiron School <github email address>
2016-07-07 16:56:10 -0400

    implemented add item

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
 
+  attr_accessor :total
+
+  def discount
+    @discount
+  end
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price)
+    self.total += price
+    #self.total = @(or no @)total + price
+    # @total += price
+    #it goes up the chain to see if its a local variable
+    #or check if its a method call
+  end
+
+end
