d2c6f118b89a1e97e4476db87fffa5024e62f590
Flatiron School <github email address>
2016-07-08 11:46:46 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 class CashRegister
 
   attr_accessor :total
+
 
   def discount
     @discount
@@ -9,14 +12,42 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @all_items = []
+    #makes it an instance variable, every time you make a new instance,
+    #new cash register, they get their own instance of that array
+    @last_price = 0
   end
 
-  def add_item(title, price)
-    self.total += price
+  def add_item(title, price, amount = 1)
+    @last_price = price
+
+    @total += price * amount
     #self.total = @(or no @)total + price
     # @total += price
     #it goes up the chain to see if its a local variable
-    #or check if its a method call
+    #and if it can't be found, looks to see if its a method call
+    amount.times do
+      @all_items << title
+    end
+  end
+
+  def apply_discount
+    if discount == 0
+      "There is no discount to apply."
+    else
+      self.total = total - total * (discount * 0.01)
+      "After the discount, the total comes to $#{self.total.to_i}."
+    end
+  end
+
+  def items
+    @all_items
+  end
+
+  def void_last_transaction
+      @total -= @last_price
   end
 
 end
+#
+# binding.pry
